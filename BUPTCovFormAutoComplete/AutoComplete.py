d9d92a9978c3651686675c2e020b652887df413d
w58296 <582961331@qq.com>
2021-11-11 14:22:51 +0800

    modify the code

--- BUPTCovFormAutoComplete/AutoComplete.py
+++ BUPTCovFormAutoComplete/AutoComplete.py
@@ -2,21 +2,17 @@ import requests
 from requests import HTTPError
 import json
 import Config
+import sys
 
 
 login_url = 'https://app.bupt.edu.cn/uc/wap/login/check'
 save_url = 'https://app.bupt.edu.cn/ncov/wap/default/save'
-location_url = 'https://api.map.baidu.com/reverse_geocoding/v3/?output=json&coordtype=wgs84ll'
 
 
-
-
-
-
-def login():
+def login(username, password):
     data = {
-        'username': 'xxxxx',
-        'password': 'xxxxx'
+        'username': username,
+        'password': password
     }
 
     r = requests.post(url=login_url, data=data)
@@ -24,7 +20,7 @@ def login():
     try:
         r.raise_for_status()
         msg = json.loads(r.text)
-        if msg.get("e") == 1:
+        if msg.get("e") == 0:
             return r.cookies
         else:
             raise HTTPError
@@ -33,26 +29,70 @@ def login():
 
 
 def cov_info_complete(cookies):
-    pass
 
-
-def get_location(url):
-
-    params = {
-        'ak':'0hYGiH3Ob5ZhV0eWzrGVXCD3bEdBCi6L',
-        'location': '39.968646,116.36244'
+    data = {
+        'address': Config.ADDRESS,
+        'area': Config.AREA,
+        'bztcyy': Config.BZTCYY,
+        'city': Config.CITY,
+        'csmjry': Config.CSMJRY,
+        'fjqszgjdq': Config.FJQSZGJDQ,
+        'geo_api_info': Config.GEO_API_INFO,
+        'glksrq': Config.GLKSRQ,
+        'gllx': Config.GLLX,
+        'gtjzzchdfh': Config.GTJZZCHDFH,
+        'gtjzzfjsj': Config.GTJZZFJSJ,
+        'ismoved': Config.ISMOVED,
+        'jcbhlx': Config.JCBHLX,
+        'jcbhrq': Config.JCBHRQ,
+        'jchbryfs': Config.JCHBRYFS,
+        'jcjgqr': Config.JCJGQR,
+        'jcwhryfs': Config.JCWHRYFS,
+        'jhfjhbcc': Config.JHFJHBCC,
+        'jhfjjtgj': Config.JHFJJTGJ,
+        'jhfjrq': Config.JHFJRQ,
+        'mjry': Config.MJRY,
+        'province': Config.PROVINCE,
+        'qksm': Config.QKSM,
+        'remark': Config.REMARK,
+        'sfcxtz': Config.SFCXTZ,
+        'sfcxzysx': Config.SFCXZYSX,
+        'sfcyglq': Config.SFCYGLQ,
+        'sfjcbh': Config.SFJCBH,
+        'sfjchbry': Config.SFJCHBRY,
+        'sfjcwhry': Config.SFJCWHRY,
+        'sfjzdezxgym': Config.SFJZDEZXGYM,
+        'sfjzxgym': Config.SFJZXGYM,
+        'sfsfbh': Config.SFSFBH,
+        'sftjhb': Config.SFTJHB,
+        'sftjwh': Config.SFTJWH,
+        'sfxk': Config.SFXK,
+        'sfygtjzzfj': Config.SFYGTJZZFJ,
+        'sfyyjc': Config.SFYYJC,
+        'sfzx': Config.SFZX,
+        'szcs': Config.SZCS,
+        'szgj': Config.SZGJ,
+        'szsqsfybl': Config.SZSQSFYBL,
+        'tw': Config.TW,
+        'xjzd': Config.XJZD,
+        'xkqq': Config.XKQQ,
+        'xwxgymjzqk': Config.XWXGYMJZQK,
+        'ymjzxgqk': Config.YMJZXGQK,
+        'zgfxdq': Config.ZGFXDQ
     }
 
-    r = requests.get(url=location_url, params=params)
+    r = requests.post(url=save_url, cookies=cookies, data=data)
 
     try:
-        r.raise_for_status()
-        print(r.url)
+        r.raise_for_status();
         print(r.text)
     except HTTPError:
-        print('请求发生错误')
+        print("请求错误")
 
 
 if __name__ == '__main__':
-    get_location(location_url)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    cookies = login(username=username, password=password)
+    cov_info_complete(cookies)
 
